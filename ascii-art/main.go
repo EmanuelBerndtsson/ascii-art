8dbedf1f2fc4f2ef591ef2e3be65be469e2533f2
emanuel <emanuel.berndtsson03@gmail.com>
2024-09-11 14:00:05 +0300

    changes and stuff

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -1,82 +1,164 @@
 package main
 
 import (
-	ascii "asciiart/functions"
-	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"os"
-	"strings"
 )
 
-func main() {
-	//  Check for correct number of arguments
-	banner := ""
-	if len(os.Args) < 2 {
-		log.Fatalln("Not enough arguments")
-	} else if len(os.Args) == 2 {
-		banner = "standard"
-	} else if len(os.Args) > 3 {
-		log.Fatalln("Too many arguments")
-	} else {
-		banner = os.Args[2]
-	}
-	textStr := os.Args[1]
+// printUsage() prints instructions on how to use the program
+func printUsage() {
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println(`./ascii-art "Hello!" [stylefile]`)
+	fmt.Println("Example:")
+	fmt.Println(`go run . "Hello!" thinkertoy`)
+	fmt.Println("If no style is provided, it defaults to 'standard'.")
+}
 
-	// Check if argument contains only ascii printable characters.
-	for _, char := range textStr {
-		if !(char >= 32 && char <= 126) {
-			fmt.Println("Please only use ascii characters")
-			os.Exit(1)
-		}
-	}
-
-	// Selects correct banner style according to user input argument.
-	bannerCall := ""
-	switch banner {
-	case "standard":
-		bannerCall = "standard.txt"
-	case "shadow":
-		bannerCall = "shadow.txt"
-	case "thinkertoy":
-		bannerCall = "thinkertoy.txt"
-	}
-
-	// Open and defer-close ascii-art file.
-	file, err := os.Open("banners/" + bannerCall)
+// getStyleFile opens and reads the ascii art file based on the provided style argument
+func getStyleFile(style string) *os.File {
+	file, err := os.Open(style + ".txt")
 	if err != nil {
-		os.Exit(2)
+		fmt.Printf("Error opening style file: %s\n", err)
+		os.Exit(1)
 	}
-	defer func() {
-		err := file.Close()
-		if err != nil {
-			log.Printf("That closing of file did not happen")
-		}
-	}()
+	return file
+}
 
-	// Read file by passing io.Reader interface to buffer Scanner, then passing it to Scan() function,
-	// then adds buffer lines outlined by '/n' which are read into 'rows' by Scanner.Text()
-	Scanner := bufio.NewScanner(file)
-	var rows []string
-	for Scanner.Scan() { // Scanner.Scan reads file and splits elements with empty string according to newling (\n) token
-		rows = append(rows, Scanner.Text()) // Scanner.text() takes the buffer content and appends string element into string slice 'rows'
+// getStyleString returns the ascii art style file content as a string
+func getStyleString(style string) string {
+	styleFile := getStyleFile(style)
+	defer styleFile.Close()
+
+	bytes, err := io.ReadAll(styleFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(bytes)
+}
+
+// getStyleBanners returns the ascii art style as slices of banners
+func getStyleBanners(style string) []string {
+	styleString := getStyleString(style)
+	styleAsBanners := []string{""}
+
+	var prevRu rune
+	char := 0
+	for j, ru := range styleString {
+		if ru == 13 || j == 0 { // skip vertical tabs and the very first line
+			continue
+		}
+
+		if ru == '\n' && prevRu == '\n' {
+			// remove last line change and
+			// start a new banner at two line changes
+			styleAsBanners[char] = styleAsBanners[char][:len(styleAsBanners[char])-1]
+			styleAsBanners = append(styleAsBanners, "")
+			char++
+		} else {
+			// otherwise add rune to current banner
+			styleAsBanners[char] += string(ru)
+		}
+		prevRu = ru
+	}
+	return styleAsBanners
+}
+
+// putInputToBanners retrieves the banners corresponding to the input characters
+// and puts them into slices representing lines of text
+func putInputToBanners(input string, banners []string) [][]string {
+	// multiple slices of banners, separated by line changes
+	bannerLines := [][]string{}
+	lineIndex := 0
+	for i, ru := range input {
+		// start a new slice of banners when encountering "\n"
+		if ru == 'n' && i > 0 && input[i-1] == '\\' {
+			bannerLines[lineIndex] = bannerLines[lineIndex][:len(bannerLines[lineIndex])-1] // remove last banner (`\`)
+
+			if !(len(bannerLines) == 1 && len(bannerLines[lineIndex]) == 0) {
+				bannerLines = append(bannerLines, []string{}) // start new line
+			}
+			lineIndex++
+			continue
+		}
+
+		if ru > 31 && ru < 127 {
+			if len(bannerLines) == 0 {
+				bannerLines = append(bannerLines, []string{})
+			}
+
+			if ru != ' ' {
+				bannerLines[lineIndex] = append(bannerLines[lineIndex], banners[ru-32])
+			} else {
+				// requested spaces are thinner than in styles
+				bannerLines[lineIndex] = append(bannerLines[lineIndex], "    \n    \n    \n    \n    \n    \n    \n    \n")
+			}
+		}
+	}
+	return bannerLines
+}
+
+// getHorizontalLines composes horizontal lines from slices of banners
+func getHorizontalLines(bannerLines [][]string) (out string) {
+	for _, line := range bannerLines {
+		rowOut := 0
+		if len(line) == 0 {
+			out += "\n"
+			continue
+		}
+		for i := 0; i < 8; i++ {
+			for _, ban := range line {
+				rowBanner := 0
+				for _, r := range ban {
+					if r == '\n' {
+						rowBanner++
+					}
+
+					if rowOut == rowBanner && r != '\n' {
+						out += string(r)
+					}
+				}
+			}
+			out += "\n"
+			rowOut++
+		}
+	}
+	return
+}
+
+// main prints the input string as banners in the selected ascii art style,
+// including line changes.
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("Provide at least a string argument")
+		printUsage()
+		os.Exit(1)
 	}
 
-	// Colonizes  asciiChars map with key value pairs, where key is decimal value of ascii-art
-	// and each element/value is one row of an ascii-art character.
-	asciiChrs := make(map[int][]string)
-	ascii.MapColonizer(asciiChrs, rows)
+	input := args[0]
+	style := "standard" // Default to 'standard' style
 
-	// Handles double '\n' characters and single '\n' character
-	outputString := ""
-	newlineStr := strings.Split(textStr, "\\n")
-	for i, val := range newlineStr {
-		if val != "" {
-			outputString += ascii.PrintAsciiArt(val, asciiChrs)
-		}
-		if i < len(newlineStr)-1 {
-			outputString += string('\n')
-		}
+	// Check if a second argument (style) is provided
+	if len(args) == 2 {
+		style = args[1]
 	}
-	fmt.Printf("%v", outputString)
+
+	if len(args) > 2 {
+		fmt.Println("to many arguments")
+		printUsage()
+		os.Exit(1)
+	}
+
+	// get the art style as banners
+	stylesAsBanners := getStyleBanners(style)
+
+	// get the lines in input as a slices of banners
+	bannerLines := putInputToBanners(input, stylesAsBanners)
+
+	// place banners to horizontal lines and print the result
+	fmt.Print(getHorizontalLines(bannerLines))
 }
